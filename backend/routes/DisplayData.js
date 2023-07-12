d7bca274162dad801fb8bf35d0b7511efc831da9
15bhavyajain <118760873+15bhavyajain@users.noreply.github.com>
2023-07-12 22:26:32 +0530

    Update DisplayData.js

--- backend/routes/DisplayData.js
+++ backend/routes/DisplayData.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 
-router.post('https://myfoodapp-kffs.onrender.com/foodData',(req,res)=>{
+router.post('/foodData',(req,res)=>{
     try {
         res.send([global.food_items,global.foodCategory])
     } catch (error) {
