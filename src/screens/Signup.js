e84e1648f27c3dbfa63b4b6b16a26160666271cd
15bhavyajain <118760873+15bhavyajain@users.noreply.github.com>
2023-07-13 15:32:32 +0530

    Update Signup.js

--- src/screens/Signup.js
+++ src/screens/Signup.js
@@ -4,13 +4,43 @@ import Navbar from "../components/Navbar";
 
 export default function Signup() {
     let navigate = useNavigate();
+    let [address, setAddress] = useState("");
   const [credentials, setcredentials] = useState({
     name: "",
     email: "",
     password: "",
     geolocation: "",
   });
+    
 
+      const handleClick = async (e) => {
+    e.preventDefault();
+    let navLocation = () => {
+      return new Promise((res, rej) => {
+        navigator.geolocation.getCurrentPosition(res, rej);
+      });
+    }
+    let latlong = await navLocation().then(res => {
+      let latitude = res.coords.latitude;
+      let longitude = res.coords.longitude;
+      return [latitude, longitude]
+    })
+    // console.log(latlong)
+    let [lat, long] = latlong
+    console.log(lat, long)
+    const response = await fetch("https://myfoodapp-kffs.onrender.com/api/getlocation", {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({ latlong: { lat, long } })
+
+    });
+    const { location } = await response.json()
+    console.log(location);
+    setAddress(location);
+    setCredentials({ ...credentials, [e.target.name]: location })
+  }
   const handleSubmit = async (e) => {
     e.preventDefault();
     console.log(JSON.stringify({name: credentials.name,email: credentials.email,
@@ -46,73 +76,40 @@ export default function Signup() {
 
   return (
     <> 
-    <Navbar/>
-    <div className="container m-3 ">
-      <form onSubmit={handleSubmit}>
-        <div className="mb-3">
-          <label htmlFor="name" className="form-label">
-            Name
-          </label>
-          <input
-            type="text"
-            className="form-control"
-            name="name"
-            value={credentials.name}
-            onChange={onChange}
-          />
-        </div>
-        <div className="mb-3">
-          <label htmlFor="exampleInputEmail1" className="form-label">
-            Email address
-          </label>
-          <input
-            type="email"
-            className="form-control"
-            id="exampleInputEmail1"
-            aria-describedby="emailHelp"
-            name="email"
-            value={credentials.email}
-            onChange={onChange}
-          />
-          <div id="emailHelp" className="form-text">
-            We'll never share your email with anyone else.
-          </div>
-        </div>
-        <div className="mb-3">
-          <label htmlFor="exampleInputPassword1" className="form-label">
-            Password
-          </label>
-          <input
-            type="password"
-            className="form-control"
-            id="exampleInputPassword1"
-            name="password"
-            value={credentials.password}
-            onChange={onChange}
-          />
-        </div>
+    
+        <div style={{ backgroundImage: 'url("https://images.pexels.com/photos/1565982/pexels-photo-1565982.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1")', backgroundSize: 'cover',height: '100vh' }}>
+      <div>
+      <Navbar />
+      </div>
 
-        <div className="mb-3">
-          <label htmlFor="name" className="form-label">
-            Address
-          </label>
-          <input
-            type="text"
-            className="form-control"
-            name="geolocation"
-            value={credentials.geolocation}
-            onChange={onChange}
-          />
+        <div className='container' >
+          <form className='w-50 m-auto mt-5 border bg-dark border-success rounded' onSubmit={handleSubmit}>
+            <div className="m-3">
+              <label htmlFor="name" className="form-label">Name</label>
+              <input type="text" className="form-control" name='name' value={credentials.name} onChange={onChange} aria-describedby="emailHelp" />
+            </div>
+            <div className="m-3">
+              <label htmlFor="email" className="form-label">Email address</label>
+              <input type="email" className="form-control" name='email' value={credentials.email} onChange={onChange} aria-describedby="emailHelp" />
+            </div>
+            <div className="m-3">
+              <label htmlFor="address" className="form-label">Address</label>
+              <fieldset>
+                <input type="text" className="form-control" name='address' placeholder='"Click below for fetching address"' value={address} onChange={(e)=>setAddress(e.target.value)} aria-describedby="emailHelp" />
+              </fieldset>
+            </div>
+            <div className="m-3">
+              <button type="button" onClick={handleClick} name="geolocation" className=" btn btn-success">Click for current Location </button>
+            </div>
+            <div className="m-3">
+              <label htmlFor="exampleInputPassword1" className="form-label">Password</label>
+              <input type="password" className="form-control" value={credentials.password} onChange={onChange} name='password' />
+            </div>
+            <button type="submit" className="m-3 btn btn-success">Submit</button>
+            <Link to="/login" className="m-3 mx-1 btn btn-danger">Already a user</Link>
+          </form>
         </div>
-
-        <button type="submit" className="m-3 btn btn-success">
-          Submit
-        </button>
-        <Link to="/login" className="m-3 btn btn-danger">
-          Already a user
-        </Link>
-      </form>
-    </div>
+      </div>
     </>
   );
   
